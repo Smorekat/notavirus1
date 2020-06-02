256cadcb8171b83f69898854a51eb14d2821850f
Smorekat <65438464+Smorekat@users.noreply.github.com>
2020-06-01 23:02:55 -0700

    s
    aaaaaaaaaaaa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CodeBruh
+{
+    class Program
+    {
+        public static int s;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("bruh");
+                s += 1;
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
